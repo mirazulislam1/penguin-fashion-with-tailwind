df7758ce3d10ff4462c3cf53e370ecd145a507ad
unknown <you@example.com>
2022-08-18 09:18:38 +0600

    new add

---
+++ js/penguin.js
@@ -0,0 +1,8 @@
+const manJacket = document.getElementById('man-jacket');
+const man = manJacket.innerText;
+console.log(man);
+
+document.getElementById('penguin-tailwind').addEventListener('click', function(){
+    const winterPenguin = document.getElementById('winter-penguin');
+    winterPenguin.innerText = 'I AM PENGUIN';
+})
